18671e13c999a52f8db2f2b8d4e9d5c18e30f4e4
Zuodian Hu <huzuodian@gmail.com>
2019-10-20 16:20:34 -0500

    feat(algo): make ripple underlying array read-only

--- include/ripple.hpp
+++ include/ripple.hpp
@@ -15,8 +15,18 @@ namespace effect {
      * \tparam num_periods The number of periods in a single ripple object.
      */
     template<typename T, std::size_t period, std::size_t num_periods>
-    class ripple : public std::array<T,period*num_periods> {
+    class ripple : std::array<T,period*num_periods> {
         public:
+            using parent_type = std::array<T,period*num_periods>;
+
+            using parent_type::const_iterator;
+            using parent_type::const_reverse_iterator;
+
+            using parent_type::cbegin;
+            using parent_type::cend;
+            using parent_type::crbegin;
+            using parent_type::crend;
+
             /**
              * Construct a ripple object.
              */
