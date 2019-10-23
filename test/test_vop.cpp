baa4b71bcbd13a562bc9bef99652ccf91f543b57
Zuodian Hu <huzuodian@gmail.com>
2019-10-23 15:26:12 -0500

    feat(test): add test for mixed-sign addition

--- test/test_vop.cpp
+++ test/test_vop.cpp
@@ -43,4 +43,17 @@ TEST_CASE( "vector sum produces proper sum", "[sum]" ) {
         CHECK( result.at( 3 ) == -12 );
         CHECK( result.at( 4 ) == -15 );
     }
+    SECTION( "mixed-sign integrals" ) {
+        std::vector<int> lhs{ -1,  2, -3, -4,   5 };
+        std::vector<int> rhs{ -2, -4,  6, -8, -10 };
+        std::vector<int> result;
+
+        vop::sum( std::back_inserter( result ), lhs.cbegin(), rhs.cbegin(), lhs.size() );
+
+        CHECK( result.at( 0 ) == -3 );
+        CHECK( result.at( 1 ) == -2 );
+        CHECK( result.at( 2 ) == 3 );
+        CHECK( result.at( 3 ) == -12 );
+        CHECK( result.at( 4 ) == -5 );
+    }
 }
