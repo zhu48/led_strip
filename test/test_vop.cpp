a0ea944e3131761d16a515407573e4a1cf4890e1
Zuodian Hu <huzuodian@gmail.com>
2019-10-23 15:23:58 -0500

    feat(test): test adding negative integrals

--- test/test_vop.cpp
+++ test/test_vop.cpp
@@ -17,15 +17,30 @@ TEST_CASE( "vector sum produces properly-sized result container", "[sum][size]" 
 }
 
 TEST_CASE( "vector sum produces proper sum", "[sum]" ) {
-    std::vector<int> lhs{ 1, 2, 3, 4, 5 };
-    std::vector<int> rhs{ 2, 4, 6, 8, 10 };
-    std::vector<int> result;
+    SECTION( "positive integrals" ) {
+        std::vector<int> lhs{ 1, 2, 3, 4, 5 };
+        std::vector<int> rhs{ 2, 4, 6, 8, 10 };
+        std::vector<int> result;
 
-    vop::sum( std::back_inserter( result ), lhs.cbegin(), rhs.cbegin(), lhs.size() );
+        vop::sum( std::back_inserter( result ), lhs.cbegin(), rhs.cbegin(), lhs.size() );
 
-    CHECK( result.at( 0 ) == lhs.at( 0 ) + rhs.at( 0 ) );
-    CHECK( result.at( 1 ) == lhs.at( 1 ) + rhs.at( 1 ) );
-    CHECK( result.at( 2 ) == lhs.at( 2 ) + rhs.at( 2 ) );
-    CHECK( result.at( 3 ) == lhs.at( 3 ) + rhs.at( 3 ) );
-    CHECK( result.at( 4 ) == lhs.at( 4 ) + rhs.at( 4 ) );
+        CHECK( result.at( 0 ) == 3 );
+        CHECK( result.at( 1 ) == 6 );
+        CHECK( result.at( 2 ) == 9 );
+        CHECK( result.at( 3 ) == 12 );
+        CHECK( result.at( 4 ) == 15 );
+    }
+    SECTION( "negative integrals" ) {
+        std::vector<int> lhs{ -1, -2, -3, -4, -5 };
+        std::vector<int> rhs{ -2, -4, -6, -8, -10 };
+        std::vector<int> result;
+
+        vop::sum( std::back_inserter( result ), lhs.cbegin(), rhs.cbegin(), lhs.size() );
+
+        CHECK( result.at( 0 ) == -3 );
+        CHECK( result.at( 1 ) == -6 );
+        CHECK( result.at( 2 ) == -9 );
+        CHECK( result.at( 3 ) == -12 );
+        CHECK( result.at( 4 ) == -15 );
+    }
 }
