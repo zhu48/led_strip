3bc00a73da246affe7ae415d915a62c3a7243121
Zuodian Hu <huzuodian@gmail.com>
2019-10-28 18:50:50 -0500

    feat(leds): mirror purple flames

--- src/main.cpp
+++ src/main.cpp
@@ -31,17 +31,28 @@ void init_palette(
     constexpr auto cycle = util::sine<
         T,
         cycle_size,
-        std::numeric_limits<T>::max()/2,
-        std::numeric_limits<T>::max()/2
+        std::numeric_limits<T>::max()/5,
+        std::numeric_limits<T>::max()/5
     >( cycle_size / 4 );
 
     auto r_end = std::copy_n( cycle.cbegin(), cycle_size/2, red.begin() );
     auto g_end = vop::sample<2>( green.begin(), cycle.cbegin(), cycle.cbegin() + cycle_size/2 );
-    auto b_end = vop::sample<4>( blue.begin(), cycle.cbegin(), cycle.cbegin() + cycle_size/2 );
+    auto b_end = vop::sample<1>( blue.begin(), cycle.cbegin(), cycle.cbegin() + cycle_size/2 );
 
     std::fill( r_end, red.end(), 0 );
     std::fill( g_end, green.end(), 0 );
     std::fill( b_end, blue.end(), 0 );
+}
+
+template<typename T>
+void do_average( T& arr ) {
+    for ( gsl::index i = 0; i < arr[0]->size(); ++i ) {
+        typename std::remove_reference_t<decltype(*arr[0])>::value_type sum = 0;
+        for ( gsl::index j = 1; j < arr.size(); ++j ) {
+            sum += (*arr[j])[i];
+        }
+        (*arr[0])[i] = sum / (arr.size()-1);
+    }
 }
 
 void iterate() {
@@ -59,33 +70,46 @@ int main( int argc, char* argv[] ) {
     std::array<std::uint8_t,3000> palette_blue;
     init_palette<2000>( palette_red, palette_green, palette_blue );
 
-    std::array<std::size_t,3000> fire{ 0 };
+    std::array<std::size_t,280> fire{ 0 };
 
     std::array<std::uint8_t,300> frame_g;
     std::array<std::uint8_t,300> frame_r;
     std::array<std::uint8_t,300> frame_b;
 
     std::mt19937 rd;
-    std::uniform_int_distribution dist{ 0, 6 };
+    std::uniform_int_distribution dist{ 0, 15 };
 
     led::ws2812b dev( *pwb_1 );
     while ( true ) {
         if ( run ) {
-            effect::doom_fire( fire.begin(), fire.end(), 3000-1, rd, dist );
+            effect::doom_fire(
+                fire.begin(),
+                fire.begin() + 140,
+                3000-1,
+                rd,
+                dist
+            );
+            effect::doom_fire(
+                fire.rbegin(),
+                fire.rbegin() + 140,
+                3000-1,
+                rd,
+                dist
+            );
 
-            vop::sample_by_index<10>(
+            vop::sample_by_index<1>(
                 frame_g.begin(),
                 fire.cbegin(),
                 fire.cend(),
                 palette_green.cbegin()
             );
-            vop::sample_by_index<10>(
+            vop::sample_by_index<1>(
                 frame_r.begin(),
                 fire.cbegin(),
                 fire.cend(),
                 palette_red.cbegin()
             );
-            vop::sample_by_index<10>(
+            vop::sample_by_index<1>(
                 frame_b.begin(),
                 fire.cbegin(),
                 fire.cend(),
